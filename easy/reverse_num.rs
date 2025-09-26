a3c1a0b519a61340c2d63fc7deb7d456b752d744
venus <jaiksinn@gmail.com>
2025-09-26 18:26:21 +0530

    DSA easy problem

--- easy/reverse_num.rs
+++ easy/reverse_num.rs
@@ -14,8 +14,27 @@ fn reverse(mut x:i32)->i32{
 		reversed
 	}
 }
+//optimal approach
+ fn safe_reverse(mut num:i32)->i32{
+	 let mut reversed:i32=0;
+	 let _negative=num<0;
+	 num=num.abs();
+	 while num>0{
+		 let digit =num%10;
+		 if let Some(new_reversed)=reversed.checked_mul(10).and_then(|r| r.checked_add(digit)){
+			 reversed=new_reversed
+		 }else{
+			 return 0;
+		 }
+		 num/=10;
+	 }
+	 reversed
+ }
+ 
 fn main(){
 	let num:i32 =12345;
-	let reversed_num=reverse(num);
+	let reversed_num=reverse(num.clone());
+	let safe_reversed=safe_reverse(num.clone());
 	println!("{}",reversed_num);
+	println!("{}",safe_reversed);
 }
